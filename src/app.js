f604cfacc78c70b42da5a2ceb98e3dce872b968e
Ryan Noronha <ryannoronha1@ryans-mbp.mynet>
2018-08-11 16:41:40 +0100

    Finished setting up server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const routeConfig = require("./config/route-config.js");
+const appConfig = require("./config/main-config.js");
 
 module.exports = app;
+
+appConfig.init();
 routeConfig.init(app);
